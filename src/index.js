3ab3efcbca3d9d9ccb4c4c3a604a89c3a99f9881
Vinicius Moreira <vinicius.moreira@godoo.com.br>
2021-03-16 12:11:27 -0300

    Conclusão do desafio

--- src/index.js
+++ src/index.js
@@ -10,19 +10,80 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const userExists = users.find(user => user.username === username);
+
+  if(!userExists){
+    return response.status(404).json({
+      error: 'User does not exists'
+    })
+  }
+
+  request.user = userExists;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(!user.pro && user.todos.length >= 10){
+    return response.status(403).json({
+      error: 'User already has 10 todos.'
+    });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const userExists = users.find(user => user.username === username);
+
+  if(!userExists){
+    return response.status(404).json({
+      error: 'User does not exists'
+    })
+  }
+
+  const { id } = request.params;
+
+  if(!validate(id)){
+    return response.status(400).json({
+      error: 'Invalid id.'
+    })
+  }
+
+  const todo = userExists.todos.find(todo => todo.id === id);
+
+  if(!todo){
+    return response.status(404).json({
+      error: 'Todo not found.'
+    })
+  }
+
+  request.todo = todo;
+  request.user = userExists;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const userExists = users.find(user => user.id === id);
+
+  if(!userExists){
+    return response.status(404).json({
+      error: 'User does not exists'
+    })
+  }
+
+  request.user = userExists;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
